468aa654882a255752823c99724d8c8edfe21713
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-01-16 09:36:24 +0100

    Initial commit

---
+++ startup.js
@@ -0,0 +1,13 @@
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (Images.find().count() == 0) {
+            for (var i = 1; i <= 22; i++) {
+                Images.insert({
+                    img_src: "img_" + i + ".jpg",
+                    img_alt: "image number " + i
+                });
+            }
+            console.log("added images:" + Images.find().count());
+        }
+    });
+}
