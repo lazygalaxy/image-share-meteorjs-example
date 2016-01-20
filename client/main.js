9ac95239131f7ee1112bb044caa5a72bcc7db172
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-01-20 22:44:35 +0100

    using iron:router

--- client/main.js
+++ client/main.js
@@ -1,5 +1,39 @@
-Session.set("imageLimit", 8);
+//routing
+Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+});
 
+Router.route('/', function () {
+    this.render('welcome', {
+        to: "main"
+    });
+});
+
+Router.route('/images', function () {
+    this.render('navbar', {
+        to: "navbar"
+    });
+    this.render('images', {
+        to: "main"
+    });
+});
+
+Router.route('/image/:_id', function () {
+    this.render('navbar', {
+        to: "navbar"
+    });
+    this.render('image', {
+        to: "main",
+        data: function () {
+            return Images.findOne({
+                _id: this.params._id
+            })
+        }
+    });
+});
+
+//infinate scrolling
+Session.set("imageLimit", 8);
 lastScrollTop = 0;
 $(window).scroll(function (event) {
     // test if we are near the bottom of the window
@@ -15,6 +49,8 @@ $(window).scroll(function (event) {
     }
 });
 
+
+//accounts config
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
 });
